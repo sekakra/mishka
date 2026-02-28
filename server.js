6ca4f73afd4c6ab7ca9a81e33b653d4e57c2ced5
sekakra <osamamen9@gmail.com>
2026-02-28 20:05:57 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 {
-  "name": "ngo-website",
+  "name": "ngo-single-page",
   "version": "1.0.0",
-  "description": "Professional NGO Website",
+  "description": "Professional Single Page NGO Website",
   "main": "server.js",
   "scripts": {
     "start": "node server.js"
   },
   "dependencies": {
     "express": "^4.18.2",
-    "ejs": "^3.1.9",
     "body-parser": "^1.20.2"
   }
 }
